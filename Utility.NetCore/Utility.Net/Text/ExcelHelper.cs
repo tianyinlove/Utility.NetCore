2f714291cdff6afe4a5d2090385f33bfd173b4bb
tianyin <tianyin0910@emoney.cn>
2020-10-27 09:38:30 +0800

    更新导出excel

--- Utility.NetCore/Utility.Net/Text/ExcelHelper.cs
+++ Utility.NetCore/Utility.Net/Text/ExcelHelper.cs
@@ -20,17 +20,18 @@ namespace Utility.Text
         /// 
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="datas"></param>
-        /// <param name="titleList"></param>
-        /// <param name="columnNames"></param>
-        /// <param name="sheetName"></param>
-        /// <param name="isProtected"></param>
+        /// <param name="datas">数据列表</param>
+        /// <param name="titleList">标题键值对，如果多单元格合并可用("A1:A2", "日期")</param>
+        /// <param name="columnNames">指定导出数据列</param>
+        /// <param name="sheetName">表名</param>
+        /// <param name="isProtected">是否加密码</param>
+        /// <param name="password">密码</param>
         /// <returns></returns>
-        public static Byte[] GetByteToExportExcel<T>(List<T> datas, List<Dictionary<string, string>> titleList, List<string> columnNames = null, string sheetName = "Sheet", bool isProtected = false)
+        public static Byte[] GetByteToExportExcel<T>(List<T> datas, List<Dictionary<string, string>> titleList, List<string> columnNames = null, string sheetName = "Sheet", bool isProtected = false, string password = "")
         {
             using (var fs = new MemoryStream())
             {
-                using (var package = CreateExcelPackage(datas, titleList, columnNames, sheetName, isProtected))
+                using (var package = CreateExcelPackage(datas, titleList, columnNames, sheetName, isProtected, password))
                 {
                     package.SaveAs(fs);
                     return fs.ToArray();
@@ -47,8 +48,9 @@ namespace Utility.Text
         /// <param name="columnNames"></param>
         /// <param name="sheetName"></param>
         /// <param name="isProtected"></param>
+        /// <param name="password">密码</param>
         /// <returns></returns>
-        static ExcelPackage CreateExcelPackage<T>(List<T> datas, List<Dictionary<string, string>> titleList, List<string> columnNames, string sheetName, bool isProtected)
+        static ExcelPackage CreateExcelPackage<T>(List<T> datas, List<Dictionary<string, string>> titleList, List<string> columnNames, string sheetName, bool isProtected, string password)
         {
             if (datas == null)
             {
@@ -61,7 +63,7 @@ namespace Utility.Text
             while (pageIndex * pageSize <= datas.Count)
             {
                 var worksheet = package.Workbook.Worksheets.Add(sheetName + pageIndex);
-                CreateExcelSheet(datas.Skip(pageIndex * pageSize).Take(pageSize).ToList(), worksheet, titleList, columnNames, isProtected);
+                CreateExcelSheet(datas.Skip(pageIndex * pageSize).Take(pageSize).ToList(), worksheet, titleList, columnNames, isProtected, password);
                 pageIndex++;
             }
             return package;
@@ -76,15 +78,13 @@ namespace Utility.Text
         /// <param name="titleList"></param>
         /// <param name="columnNames"></param>
         /// <param name="isProtected"></param>
-        static void CreateExcelSheet<T>(List<T> datas, ExcelWorksheet worksheet, List<Dictionary<string, string>> titleList, List<string> columnNames, bool isProtected)
+        /// <param name="password">密码</param>
+        static void CreateExcelSheet<T>(List<T> datas, ExcelWorksheet worksheet, List<Dictionary<string, string>> titleList, List<string> columnNames, bool isProtected, string password)
         {
-            worksheet.Cells.Style.WrapText = true;
-            worksheet.Cells.Style.ShrinkToFit = true;//单元格自动适应大小
-
-            if (isProtected)
+            if (isProtected && !string.IsNullOrEmpty(password))
             {
                 worksheet.Protection.IsProtected = true;//设置是否进行锁定
-                worksheet.Protection.SetPassword("alex");//设置密码
+                worksheet.Protection.SetPassword(password);//设置密码
                 worksheet.Protection.AllowAutoFilter = false;//下面是一些锁定时权限的设置
                 worksheet.Protection.AllowDeleteColumns = false;
                 worksheet.Protection.AllowDeleteRows = false;
@@ -101,6 +101,9 @@ namespace Utility.Text
                 worksheet.Protection.AllowSelectUnlockedCells = false;
                 worksheet.Protection.AllowSort = false;
             }
+
+            worksheet.Cells.Style.WrapText = true;
+            worksheet.Cells.Style.ShrinkToFit = true;//单元格自动适应大小
 
             foreach (Dictionary<string, string> list in titleList)
             {
